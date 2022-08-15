3b8c2e6614ef42b4b61bfced3738109542932388
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-08-15 13:19:58 +0600

    PHP Condition

---
+++ condition.php
@@ -0,0 +1,13 @@
+<?php 
+    // $result = 1 < 3;
+    // echo $result == true;
+
+    $firstName = "Chris";
+    $lastName = "Evans";
+
+    if($firstName == "chris" || $lastName == "Evans"){
+        echo "It is true";
+    }else{
+        echo "false";
+    }
+?>
