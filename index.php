b6e068da3fae3c4dad2f7c2004028b271e8c415b
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-10-08 23:51:20 +0600

    PHP Get rquest

--- index.php
+++ index.php
@@ -2,21 +2,41 @@
 
     include('./partials/header.php');
 
-    // $product_id = $_GET['productid'];
-    // $category = $_GET['category'];
-    // $limit = $_GET['limit'];
-
-    $category = filter_input(INPUT_GET, 'category', FILTER_VALIDATE_INT);
-    $limit = filter_input(INPUT_GET, 'limit', FILTER_VALIDATE_INT);
-
-    if($category == false || $limit == false){
-        die();
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        
+        if ($email == false) {
+          $status = 'Please enter a valid email address';
+        }
     }
+
+    // if (isset($_POST['login'])) {
+    //   output($_POST);
+    // }
+
 
 ?>
     <div class="container">
-            <!-- Hello -->
-        The Limit is <?= $limit ?> and it's category is <?= $category ?>.
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input class="form-control" type="text" name="email" id="email" />
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input class="form-control" type="password" name="password" id="password" />
+            </div>
+            <div class="from-group">
+                <input type="submit" name="login" value="Login" />
+            </div>
+        </form>
+    </div>
+    <div class="row">
+        <?php 
+          if (isset($status)) {
+            echo $status;
+          }
+        ?>
     </div>
     
 <?php 
